c91e7b8040dd9e9267f7ec0646c22dfb918b1458
Bekzod <bekzodusarovv@gmail.com>
2023-06-27 10:27:27 +0500

    2-v

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -3,14 +3,17 @@ import wikipedia
 
 from loader import dp
 
-
+wikipedia.set_lang("uz")
 # Echo bot
 @dp.message_handler(state=None)
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
-    wikipedia.set_lang("uz")
-    savol = message.text
-    javob = wikipedia.summary(savol)
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer('Bu mavzuga oid maqola topilmadi')
 
-    await message.answer(javob)
+
+
